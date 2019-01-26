cee6778770823c083eb8c6d5d9d583469f58cfbf
Robert Casanova <44031505+robertcasanova99@users.noreply.github.com>
2019-01-25 22:15:09 -0500

    Add files via upload

---
+++ databasecall.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "robert";
+$dbname = "facts";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, fact FROM facttable";
+$result = $conn->query($sql);
+$num_rows = mysqli_num_rows($result);
+$fetchNum = rand(1, $num_rows);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+    	if ($row["id"] == $fetchNum) {
+    		echo $row["fact"];
+    	}
+	} 
+}else {
+    echo "Empty database!";
+} 
+
+$conn->close();
+
+?>
